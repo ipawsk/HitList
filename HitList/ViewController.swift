b91628f688e3c1224f3f1ecdaffc78070ce4c667
iPaw <pawizacked@gmail.com>
2025-10-05 18:48:38 -0600

    Added Core Data project to handle data and show it in a table

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -6,14 +6,101 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var people: [NSManagedObject] = []
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    @IBAction func addNameButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            guard let textField = alert.textFields?.first,
+            let nameToSave = textField.text else {
+                return
+            }
+            
+            self.save(name: nameToSave)
+            self.tableview.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpView()
     }
+    
+    func setUpView() {
+        title = "The List"
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    func save(name : String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let manageContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: manageContext)! //Crea un objeto y lo inserta en el contexto
+        
+        let person = NSManagedObject(entity: entity, insertInto: manageContext)
+        
+        person.setValue(name, forKey: "name") // se configura el atributo , se pone el nombre exactamente como en el modelo
+        
+        do { //Los cambios se confirman en person y se guardan
+            try manageContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchPeople()
+    }
+    
+    func fetchPeople() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { // se obtiene la referencia del contenedor
+            return
+        }
+        
+        let manageContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person") // Para la obtencion de datos
+        
+        do {
+            people = try manageContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
 
-
+        }
+    }
 }
 
+// MARK: - TableView DataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = people[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+        return cell
+    }
+}
